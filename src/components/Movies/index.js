11dc2c5678d689171d58c5f238c933ade19bca28
Judhistira Natha Junior <judhistira.natha@gmail.com>
2022-09-22 08:50:10 +0000

    Update 4 files

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,22 @@
-function Movies() {
+import MovieDetails from "../MovieDetails";
+import { Route, Switch, useRouteMatch, Link } from "react-router-dom";
+
+function Movies({ movies }) {
+  const { url } = useRouteMatch();
+
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((item) => {
+          return <Link key={item.id} to={`${url}/${item.id}`}> {item.title} </Link>;
+        })}
+      </nav>
+      <Switch>
+        <Route path={`${url}/:movieId`}>
+          <MovieDetails movies={movies}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
